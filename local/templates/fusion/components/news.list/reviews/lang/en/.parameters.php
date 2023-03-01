c30d5b087716e54239ae08551960672df8d91011
Vadim Windows <hello@vadim24.ru>
2023-03-01 12:05:28 +0300

    Подправил ошибку в наименовании переменной $MESS

--- local/templates/fusion/components/news.list/reviews/lang/en/.parameters.php
+++ local/templates/fusion/components/news.list/reviews/lang/en/.parameters.php
@@ -4,10 +4,10 @@ $MESS ['T_IBLOCK_DESC_NEWS_NAME'] = "Display element title";
 $MESS ['T_IBLOCK_DESC_NEWS_PICTURE'] = "Display element preview picture";
 $MESS ['T_IBLOCK_DESC_NEWS_TEXT'] = "Display element preview text";
 
-$MES["SLIDER"] = "Slider settings";
-$MES["SLIDER_ID"] = "Slider ID";
-$MES["SLIDER_TIME"] = "Timeout";
-$MES["SLIDER_AUTOPLAY"] = "Enable autoplay";
-$MES["SLIDER_DOTS"] = "Enable pagination";
-$MES["NEWS_COUNT"] = "Number of slides per page";
+$MESS["SLIDER"] = "Slider settings";
+$MESS["SLIDER_ID"] = "Slider ID";
+$MESS["SLIDER_TIME"] = "Timeout";
+$MESS["SLIDER_AUTOPLAY"] = "Enable autoplay";
+$MESS["SLIDER_DOTS"] = "Enable pagination";
+$MESS["NEWS_COUNT"] = "Number of slides per page";
 ?>
